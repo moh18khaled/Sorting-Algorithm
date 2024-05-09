affad183d185b0bd0214a85b2b34729c94b227d3
mohamed khaled <73349404+moh18khaled@users.noreply.github.com>
2024-05-10 01:37:36 +0300

    Delete final project/final project/Debug directory

--- final project/final project/Debug/KeyPad.d
+++
@@ -1,8 +0,0 @@
-KeyPad.d KeyPad.o: .././KeyPad.c .././KeyPad.h .././Math.h \
- .././register.h
-
-.././KeyPad.h:
-
-.././Math.h:
-
-.././register.h:
--- final project/final project/Debug/SevSeg.d
+++
@@ -1,8 +0,0 @@
-SevSeg.d SevSeg.o: .././SevSeg.c .././SevSeg.h .././Math.h \
- .././register.h
-
-.././SevSeg.h:
-
-.././Math.h:
-
-.././register.h:
