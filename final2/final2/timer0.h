d27e383025da4a8bb468e8f30ba7f0e0d51db23a
mohamed khaled <73349404+moh18khaled@users.noreply.github.com>
2024-05-11 15:12:01 +0300

    Update timer0.h

--- final2/final2/timer0.h
+++ final2/final2/timer0.h
@@ -12,6 +12,20 @@
 //#define NUMBER_OF_OVERFLOWS 30  // F_CPU = 8000000   
 
 #define NUMBER_OF_OVERFLOWS 61  // F_CPU = 16000000
+
+
+/*
+
+tick = (1024/16000000)*256
+
+
+tick = 0.016384 sec 
+
+NUMBER_OF_OVERFLOWS = 1/tick
+
+*/
+
+
 #define CS00 0
 #define CS02 2
 #define FOC0 7
